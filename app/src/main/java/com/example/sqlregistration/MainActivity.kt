2979abb998846a03b3f03828a2cd61a1f1b47af8
RamanaR-2006 <ramanaramachandran14@gmail.com>
2023-07-18 16:37:40 +0530

    change

--- app/src/main/java/com/example/sqlregistration/MainActivity.kt
+++ app/src/main/java/com/example/sqlregistration/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.sqlregistration
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +14,21 @@ class MainActivity : AppCompatActivity() {
         var username = findViewById<EditText>(R.id.username)
         var password = findViewById<EditText>(R.id.password)
         var button = findViewById<Button>(R.id.button1)
+
+        button.setOnClickListener {
+            var username1 = username.text
+            var password1 = password.text
+            if (username1.isEmpty()) {
+                Toast.makeText(this,"Username is empty",Toast.LENGTH_SHORT)
+            }
+            else if(password1.isEmpty()){
+                Toast.makeText(this,"Password is empty",Toast.LENGTH_SHORT)
+            }
+            else{
+                Toast.makeText(this,"Accepted",Toast.LENGTH_SHORT)
+                val intent = Intent(this, LoginView::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
